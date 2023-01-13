529ecb91907b7b294918c029220aec19e99e3b5b
NicolaZomer <nicola.zomer@gmail.com>
2023-01-13 16:43:48 +0100

    modified plot_history

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -37,7 +37,8 @@ def plot_history(history, columns=['loss']):
         ax = axes[i] if len(columns) > 1 else axes
         ax.plot(history.history[column], label='training', color='blue', linewidth=1.5)
         ax.plot(history.history['val_'+column], label='validation', color='firebrick', linewidth=1.5)
-        ax.set_xticks(range(len(history.history['loss'])), labels=range(1, len(history.history['loss'])+1))
+        ax.set_xticks(range(len(history.history['loss'])))
+        ax.set_xticklabels(range(1, len(history.history['loss'])+1))
         ax.set_xlabel('epoch')
         ax.grid(alpha=0.5)
         ax.set_ylabel(column)
@@ -47,13 +48,22 @@ def plot_history(history, columns=['loss']):
     plt.show()
     
 
-def plot_confusion_matrix(cm, labels, annot=True, cmap='Blues'):
-
-    _, ax = plt.subplots(figsize=(12, 9))
-    ax = sns.heatmap(cm, annot=annot, xticklabels=labels, yticklabels=labels, cmap=cmap)
-    ax.set_xlabel('Predicted label')
-    ax.set_ylabel('True label')
-    ax.set_title('Confusion matrix')
+def plot_confusion_matrix(cm, labels, annot=True, cmap='Blues', normalize=False, saveit=False, model_name=''):
+    _, ax = plt.subplots(figsize=(21, 18))
+    
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        ax = sns.heatmap(cm, vmin=0, vmax=1, annot=annot, xticklabels=labels, yticklabels=labels, cmap=cmap, fmt=".2f")
+        
+    else:
+        ax = sns.heatmap(cm, annot=annot, xticklabels=labels, yticklabels=labels, cmap=cmap)
+    
+    ax.set_xlabel('Predicted label', fontsize=15)
+    ax.set_ylabel('True label', fontsize=15)
+    ax.set_title('Confusion matrix', fontsize=18)
 
     plt.tight_layout()
+    
+    if saveit:
+        plt.savefig(f'figures/cm_{model_name}.png')
     plt.show()
