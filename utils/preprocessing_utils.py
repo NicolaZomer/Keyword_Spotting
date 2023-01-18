52e328d1cff3e6168ce126895f4c87ee8925a292
NicolaZomer <nicola.zomer@gmail.com>
2023-01-18 09:53:27 +0100

    added resize to preprocessing

--- utils/preprocessing_utils.py
+++ utils/preprocessing_utils.py
@@ -221,7 +221,7 @@ def get_mfcc(
     return full_feat
 
 
-def load_and_preprocess_data(file_name, file_label, data_path_=data_path, apply_background_noise=False, noise_dict=None, features=1):
+def load_and_preprocess_data(file_name, file_label, data_path_=data_path, apply_background_noise=False, noise_dict=None, features=1, resize=False):
     '''
     features:
     - 1 for MFCC features (default)
@@ -256,6 +256,10 @@ def load_and_preprocess_data(file_name, file_label, data_path_=data_path, apply_
     
     else:
         data_features = data
+        
+    # resize feature vector
+    if resize:
+        data_features = np.resize(data_features, (50, 50))
     
     # TensorFlow takes as input 32-bit floating point data
     return data_features.astype(np.float32)
